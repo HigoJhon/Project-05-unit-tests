ec3afbd330784f04aded24d6b204123301f071dd
Higo Jhon <higojhon98@gmail.com>
2022-09-20 18:30:37 -0300

    Implementando a função vqv

--- src/vqv.js
+++ src/vqv.js
@@ -18,6 +18,13 @@
   Caso a função seja chamada sem nenhum parâmetro, o valor undefined deve ser retornado.
 */
 
-const vqv = () => {};
+const vqv = (nome, idade) => {
+  if (typeof nome === 'string' && typeof idade === 'number') {
+    return `Oi, meu nome é ${nome}!
+Tenho ${idade} anos,
+trabalho na Trybe e mando muito em programação!
+#VQV!`;
+  } return undefined;
+};
 
 module.exports = vqv;
